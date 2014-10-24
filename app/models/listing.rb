d7d4c4c083024afea3d2cc14bf636185db2249c1
melonas13 <ReadMe>
2014-10-24 02:44:54 +0100

    Fixed my error on models listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,11 +7,11 @@ class Listing < ActiveRecord::Base
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                       :path => ":style/:id_:filename"
-                      
-                    validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
-  end
+                  end    
+                     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
 end
                 
-                  
+                 
 
 
