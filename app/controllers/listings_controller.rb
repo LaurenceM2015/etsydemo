d45ea8489067f900c1ece570d064f37d219be1bb
melonas13 <laurencemalonga@hotmail.com>
2015-04-13 11:42:11 +0100

    Added a Pagination

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,26 +1,18 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-   before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
-   before_filter :check_user, only: [:edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
 
-
-   #Here I am telling the controller that I have added a new page for a the seller
-   
-   def seller
-       @listings = Listing.where(user: current_user).order("created_at DESC")
-     end
+  def seller
+    
+    @listings = Listing.all.order("created_at DESC")
+  end
 
   # GET /listings
   # GET /listings.json
+  # GET /listings. and adding a paginate page
   def index
-    # this code check if the category parmeter was pass
-    if params[:category].blank?
-    @listings = Listing.all.order("created_at DESC")
-  else
-    @category_id = Category.find_by(name: params[:category]).id
-    @listings = Listing.where(category_id: @category_id).order("created_at DESC")
-  end
-    
+     @listings = Listing.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
   end
 
   # GET /listings/1
@@ -42,27 +34,27 @@ class ListingsController < ApplicationController
   def create
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
-    
-    if current_user.recipient.blank?
-    Stripe.api_key = ENV["STRIPE_API_KEY"]
-    token = params[:stripeToken]
-    
-    recipient = Stripe::Recipient.create(
-          :name => current_user.name,
-          :type => "individual",
-          :bank_account => token
-          )
-       # end
 
-        current_user.recipient = recipient.id
-        current_user.save
-end
+    if current_user.recipient.blank?
+      Stripe.api_key = ENV["STRIPE_API_KEY"]
+      token = params[:stripeToken]
+
+      recipient = Stripe::Recipient.create(
+        :name => current_user.name,
+        :type => "individual",
+        :bank_account => token
+        )
+
+      current_user.recipient = recipient.id
+      current_user.save
+    end
+
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-        format.json { render :show, status: :created, location: @listing }
+        format.json { render action: 'show', status: :created, location: @listing }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -74,9 +66,9 @@ end
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -87,26 +79,25 @@ end
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.html { redirect_to listings_url }
       format.json { head :no_content }
     end
   end
 
   private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_listing
-        @listing = Listing.find(params[:id])
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    # for security reason we need to add everytime we add a new form field because our app need to know this an additional parameter
     def listing_params
-          params.require(:listing).permit(:name, :category_id, :description, :price, :image)
-        end
-        
+      params.require(:listing).permit(:name, :description, :price, :image)
+    end
+
     def check_user
       if current_user != @listing.user
-         redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
-        end
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
       end
     end
+end
